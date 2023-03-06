b4a28742c8b8f9dd80434cabfb13363dd6c23316
Luis Leal <luisrrleal@gmail.com>
2023-03-05 19:52:31 -0600

    Add landmark list

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,22 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Luis Leal on 05/03/23.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarks) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
