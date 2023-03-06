a7c64102ce99cf5b7d5086870a7095ac2f26682f
Luis Leal <luisrrleal@gmail.com>
2023-03-05 19:30:50 -0600

    First tutorial completed

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -13,8 +13,11 @@ struct ContentView: View {
         VStack {
             MapView()
                 .frame(height: 300)
+                .ignoresSafeArea(edges:.top)
             
             CircleImage()
+                .offset(y: -130)
+                .padding(.bottom,-130)
             
             VStack(alignment: .leading) {
                     Text("Hello, Luis!")
@@ -27,8 +30,17 @@ struct ContentView: View {
                     Text("California")
                         .font(.subheadline)
                 }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("About turtle rock")
+                    .font(.title2)
+                Text("Descriptive text goes here")
                 }
             .padding()
+            Spacer()
         }
     }
 }
